6813b98fa191d88b495f17121dae49ee132a5ca3
Philip Kallberg <philip@cloud66.com>
2013-08-26 12:06:44 +0100

    update

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -1,8 +1,8 @@
 class CounterController < ApplicationController
 	def counter
-		data = IO.readlines("/lib/tasks/log.txt")
-		data.each do |line|
-		 	@cpu = line
-		end
+		#data = IO.readlines("/lib/tasks/log.txt")
+		#data.each do |line|
+		# 	@cpu = line
+		#end
 	end
 end
